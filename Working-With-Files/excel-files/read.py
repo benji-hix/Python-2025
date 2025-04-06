c0a3e2fe75e2c58d96f2df06168f9de8cefd87a3
Benji Hix <benji.hix@outlook.com>
2025-04-06 13:07:31 -0700

    Refactored read.py
    - Error handling
    
    - Modularization

--- Working-With-Files/excel-files/read.py
+++ Working-With-Files/excel-files/read.py
@@ -3,18 +3,23 @@
 from openpyxl import Workbook, load_workbook
 from pathlib import Path
 
-def get_path() -> Path:
+wkbk_name = ''
+
+def clean_str(str: str) -> str:
+    return str.strip("'").strip('"')
+
+def get_dir_path() -> Path:
     while True:
         try:
-            dir_str = input('\n| Paste directory path:\n| ').strip('"').strip("'")
+            dir_str = clean_str(input('\n| Paste directory path:\n| '))
             target_dir = Path(dir_str)
             if not target_dir.is_dir():
-                raise FileNotFoundError(f'| The directory {dir_str} does not exist.')
-            print('| Directory loaded successfully.')
-            break
-
+                raise FileNotFoundError(f'| {dir_str} not found.')
+            else:
+                print('| Directory loaded successfully.')
+                break
         except FileNotFoundError as e:
-            print(f'Error: {e}')
+            print(f'| Error: {e}')
 
     return target_dir
 
@@ -23,37 +28,58 @@ def print_dir_files(dir: Path) -> None:
         f for f in dir.iterdir() 
         if f.is_file() and f.suffix == '.xlsx' and not f.name.startswith('~$')
         ] 
-    print('| Files found:')
-    for f in dir_files:
-        print(f'    | {f.name}')
+    if not dir_files:
+        print('| No files found.')
+    else:
+        print('| Files found:')
+        for f in dir_files:
+            print(f'|····{f.name}')
     return None
 
-def clean_str(str: str) -> str:
-    return str.strip("'").strip('"')
-
-def get_wkbk(dir: Path) -> str:
+def get_wkbk_path(dir: Path) -> str:
     print_dir_files(dir)
-    wkbk_name = clean_str(input('\n| Select file:\n| '))
-    if not wkbk_name.endswith('.xlsx'):
-        wkbk_name += '.xlsx'
-    return wkbk_name
+    while True:
+        try:
+            wkbk_name = clean_str(input('\n| Select file:\n| '))
+            if not wkbk_name.endswith('.xlsx'):
+                wkbk_name += '.xlsx'
+            wkbk_path = dir / wkbk_name
+            if not wkbk_path.exists():
+                raise FileNotFoundError(f'| {wkbk_name} not found.')
+            else:
+                break
+        except FileNotFoundError as e:
+            print(f'| Error: {e}')
+    return wkbk_path
 
-def get_wksheet(wkbk_name: str, dir: Path) -> Workbook:
-    wkbk_path = str(dir) + '\\' + wkbk_name
+def open_wkbk(wkbk_path: Path) -> Workbook:
+    print(f'| {wkbk_name} loaded successfully')
+    return load_workbook(str(wkbk_path))
 
-
-    workbook = load_workbook(wkbk_path)
-    print(f'\n| {wkbk_name} loaded successfully.')
+def print_wksheets(wkbk: Workbook) -> None:
     print('| Worksheets found:')
-    for s in workbook.sheetnames:
-        print(f'    | {s}')
+    for s in wkbk.sheetnames:
+        print(f'|····{s}')
+    return None
 
-    sheet_name = input('\n| Select worksheet:\n| ')
-    sheet = workbook[f'{sheet_name}']
+def open_wksheet(wkbk_path: Path) -> Workbook:
+    current_wkbk = open_wkbk(wkbk_path)
+    print_wksheets(current_wkbk)
+    while True:
+        try:
+            sheet_name = clean_str(input('\n| Select worksheet:\n| '))
+            if sheet_name not in current_wkbk.sheetnames:
+                raise FileNotFoundError(f'{sheet_name} not found.')
+            else:
+                sheet = current_wkbk[f'{sheet_name}']
+                break
+        except FileNotFoundError as e:
+            print(f'| Error: {e}')
+        
     print(f'| {sheet_name} loaded successfully.')
     return sheet
 
-def open_workbook(wkbk_name: str='') -> Workbook:
+def open_workbook() -> Workbook:
     """
     Open correct file and return
 
@@ -64,10 +90,8 @@ def open_workbook(wkbk_name: str='') -> Workbook:
         Workbook: Workbook to work with
     """
 
-    dir = get_path()
+    dir_path = get_dir_path()
+    wkbk_path = get_wkbk_path(dir_path)
+    current_wksht = open_wksheet(wkbk_path) 
 
-    wkbk_name = get_wkbk(dir)
-
-    wksht = get_wksheet(wkbk_name, dir) 
-
-    print('\n')
+    return current_wksht
